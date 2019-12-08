2c55933428493b81e963b8e9ac8dd8981e5bfdf9
Henry Han <31779862+GitHanb@users.noreply.github.com>
2019-12-07 21:43:08 -0800

    Update app.js
    changed to arrow functions and updated the alert message

--- app.js
+++ app.js
@@ -13,51 +13,55 @@ let computerSelection = '';
 let winner = '';
 let gameIsRunnning = false;
 
-const getPlayerChoice = function() {
-    const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}?`, '').toUpperCase();
-    if (selection !== ROCK && 
-        selection !== PAPER && 
-        selection !== SCISSORS) {
-        alert('Invalid choice! We chose Rock for you!');
-        return DEFAULT_USER_CHOICE;
-    }
-    return selection;
-}
+const getPlayerChoice = () => {
+  const selection = prompt(`${ROCK}, ${PAPER}, ${SCISSORS}?`, '').toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert('Invalid choice! We chose Rock for you!');
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
 
-const getComputerChoice = function() {
-    const randomValue = Math.random();
-    if (randomValue < 0.34) {
-        return ROCK;
-    } else if (randomValue < 0.67) {
-        return PAPER;
-    } else {
-        return SCISSORS;
-    }
-}
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
 
-const getWinner = function (cChoice, pChoice) {
-    if(cChoice === pChoice) {
-        return RESULT_DRAW;
-    } else if (cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK)
-    {
-        return RESULT_PLAYER_WINS;
-    } else {
-        return RESULT_COMPUTER_WINS;
-    }
-}
+const getWinner = (cChoice, pChoice) => {
+  return cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+};
 
-const start = function() {
-    if (gameIsRunnning) {
-        return;
-    }
-    gameIsRunnning = true;
-    console.log('Game is starting...');
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    console.log('Player: '+playerSelection);
-    console.log('Computer: '+computerSelection);
-    winner = getWinner(computerSelection, playerSelection);
-    console.log(winner);
+const start = () => {
+  if (gameIsRunnning) {
+    return;
+  }
+  gameIsRunnning = true;
+  console.log('Game is starting...');
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+  winner = getWinner(computerSelection, playerSelection);
+  let message = `You picked ${playerSelection}, computer picked ${computerSelection}, `;
+  if (winner === RESULT_DRAW) {
+    message = message + `therefore you had a draw.`;
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + `therefore you won.`;
+  } else {
+    message = message + `therefore you lost.`;
+  }
+  alert(message);
+  gameIsRunnning = false;
 };
 
 startGameBtn.addEventListener('click', start);
